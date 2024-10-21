87cc8f73ee5ed65561d3b05d8d89a7a1c880ba91
Daniel Wagner-Hall <daniel@codeyourfuture.io>
2024-10-21 18:33:38 +0100

    Fix number rounding (#45)
    This is meant to round up, not round down.

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -1,9 +1,9 @@
-const num = 56.4567;
+const num = 56.5678;
 
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // Create a variable called wholeNumberPart and assign to it an expression that evaluates to 56 ( the whole number part of num )
-// Create a variable called decimalPart and assign to it an expression that evaluates to 0.4567 ( the decimal part of num )
+// Create a variable called decimalPart and assign to it an expression that evaluates to 0.5678 ( the decimal part of num )
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
 
 // Log your variables to the console to check your answers

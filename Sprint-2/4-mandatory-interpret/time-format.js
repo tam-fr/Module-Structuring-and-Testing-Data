d4f95074201c463da466bc7dd778d8097ff461f6
Tam <tam-huynh@outlook.fr>
2025-10-22 11:20:56 +0200

    sprint-1 and sprint-2 done

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -7,7 +7,8 @@ function formatTimeDisplay(seconds) {
   const totalMinutes = (seconds - remainingSeconds) / 60;
   const remainingMinutes = totalMinutes % 60;
   const totalHours = (totalMinutes - remainingMinutes) / 60;
-
+  // console.log(totalHours);
+  console.log(remainingSeconds);
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
 
@@ -17,18 +18,19 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// =============> write your answer here ---> 3 times
 
-// Call formatTimeDisplay with an input of 61, now answer the following:
+// Call formatTimeDisplay with an input of 61, now answer the following: ---> 00:01:01
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here ---> 61
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// =============> write your answer here  ---> 0
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+console.log(formatTimeDisplay(61));

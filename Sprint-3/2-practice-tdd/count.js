dd1c099f764d13fe7b101a9e223ef589bdc7d304
Tam <tam-huynh@outlook.fr>
2025-10-23 21:18:42 +0200

    count modified

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,15 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  let num = 0;
+  if (findCharacter) {
+    for (let i = 0; i < stringOfCharacters.length; i++) {
+      if (stringOfCharacters[i] === findCharacter) {
+        num++;
+      }
+    }
+    return `${findCharacter} appears ${num} time(s).`;
+  } else return 0;
 }
+
+console.log(countChar("Hello", "l"));
 
 module.exports = countChar;

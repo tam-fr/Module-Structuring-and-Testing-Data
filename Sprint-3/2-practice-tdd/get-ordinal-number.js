30318b25f4da54f9a3a6db435f0d1b4fd71ccab5
Tam <tam-huynh@outlook.fr>
2025-10-23 21:26:18 +0200

    ordinalNumber modified

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,11 @@
 function getOrdinalNumber(num) {
+  if (num > 1 || num < 1) {
+    return "No 1st";
+  }
+
   return "1st";
 }
+
+console.log(getOrdinalNumber(1));
 
 module.exports = getOrdinalNumber;
